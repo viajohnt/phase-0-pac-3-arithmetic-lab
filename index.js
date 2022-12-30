5be9971526ec56e11eb4eb4d3ea4b2e36f07bc6a
John Truong <viajohnt@gmail.com>
2022-12-30 15:05:29 -0700

    commit tree d939103b5b44957e5fc1b7b7bef387ca0726fd7a

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a+b;
+}
+function subtract() {
+    return a-b;
+}
+function multiply() {
+    return a*b;
+}
+function divide() {
+    return a/b;
+}
+function increment(n) {
+    return n + 1;
+  }
+function decrement(n) {
+    return n - 1;
+  }
+function makeInt(n) {
+    return parseInt(n);
+  }
+function preserveDecimal(n) {
+    return parseFloat(n);
+  }
